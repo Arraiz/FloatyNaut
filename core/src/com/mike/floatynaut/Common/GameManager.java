8a5f43c6bdf94448e9f7e313c8570f0662139805
arraiz <mikeldiezgarcia@gmail.com>
2017-11-19 22:52:38 +0100

    added game manager

---
+++ core/src/com/mike/floatynaut/Common/GameManager.java
@@ -0,0 +1,10 @@
+package com.mike.floatynaut.Common;
+
+/**
+ * Created by mikeldiez on 19/11/2017.
+ */
+
+public class GameManager {
+    //singleton class for accesing the Local Storage of the phone
+
+}
